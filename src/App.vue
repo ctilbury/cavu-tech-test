25696a3bd12971a3c4b7ac82aa9eb1d9ba6592f8
ctilbury <ctilbury@users.noreply.github.com>
2022-09-08 22:19:40 +0100

    get flight data from api and format it as needed

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,66 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import FlightBoard from './components/FlightBoard.vue'
 
 export default {
   name: 'App',
   components: {
     FlightBoard
+  },
+  data() {
+    return {
+      departures: []
+    }
+  },
+  methods: {
+    formatFlightData(allDepartures) {
+      const formattedFlightData = allDepartures.map(flight => {
+        const formattedFlight = {}
+
+        formattedFlight.id = uuidv4()
+
+        flight.scheduledDepartureDateTime ?
+          formattedFlight.departureTime = flight.scheduledDepartureDateTime :
+          formattedFlight.departureTime = ''
+
+        flight.arrivalAirport && flight.arrivalAirport.cityName ?
+          formattedFlight.cityName = flight.arrivalAirport.cityName : 
+          formattedFlight.cityName = ''
+
+        flight.arrivalAirport && flight.arrivalAirport.code ?
+          formattedFlight.airportCode = flight.arrivalAirport.code : 
+          formattedFlight.airportCode = ''
+
+        flight.airline && flight.airline.name ?
+          formattedFlight.airline = flight.airline.name :
+          formattedFlight.airline = ''
+
+        flight.departureGate && flight.departureGate.number ? 
+          formattedFlight.departureGate = flight.departureGate.number : 
+          formattedFlight.departureGate = ''
+
+        flight.status ? 
+          formattedFlight.status = flight.status :
+          formattedFlight.status = ''
+
+        return formattedFlight
+      })
+      this.departures = formattedFlightData
+    },
+    async getFlightData() {
+      try {
+        const response = await axios.get('https://6315ae3e5b85ba9b11e4cb85.mockapi.io/departures/Flightdata')
+        this.formatFlightData(response.data.allDepartures)
+      } catch(error) {
+        console.log(`error: ${error}`)
+      }
+    }
+  },
+  async mounted() {
+    await this.getFlightData()
   }
 }
 </script>
