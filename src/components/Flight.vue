d7b60a84a2abe1fdc73caa545dbfc42ecbb33a09
ctilbury <ctilbury@users.noreply.github.com>
2022-09-09 12:32:46 +0100

    add colours to status label

--- src/components/Flight.vue
+++ src/components/Flight.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="flight">
-    <span>{{ departureTime }}</span>
-    <span class="yellow">{{ cityName }}</span>
-    <span>{{ airportCode }}</span>
-    <span>{{ airline }}</span>
-    <span class="yellow">{{ departureGate }}</span>
-    <span>{{ status }}</span>
+    <span>
+      {{ departureTime }}
+    </span>
+    <span class="yellow-text">
+      {{ cityName }}
+    </span>
+    <span>
+      {{ airportCode }}
+    </span>
+    <span>
+      {{ airline }}
+    </span>
+    <span class="yellow-text">
+      {{ departureGate }}
+    </span>
+    <span
+      class="status-label"
+      :class="{
+        'orange' : statusOrange,
+        'blue' : statusBlue,
+        'green' : statusGreen
+      }"
+    >
+      {{ status }}
+    </span>
   </div>
 </template>
 
@@ -37,12 +56,24 @@ export default {
       type: String,
       default: ''
     }
+  },
+  computed: {
+    statusOrange: function() {
+      return this.status.toLowerCase().includes('final call') || this.status.toLowerCase().includes('flight closing')
+    },
+    statusBlue: function() {
+      return this.status.toLowerCase().includes('go to gate') || this.status.toLowerCase().includes('boarding')
+    },
+    statusGreen: function() {
+      return this.status.toLowerCase().includes('departed')
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
 @import '../assets/styles/mixins.scss';
+@import '../assets/styles/variables.scss';
 
 .flight {
   @include column-layout;
@@ -56,8 +87,31 @@ export default {
   span {
     color: white;
     
-    &.yellow {
-      color: #F5D836;
+    &.yellow-text {
+      color: $yellow;
+    }
+
+    &.status-label {
+      background-color: white;
+      color: $almost-black;
+      box-shadow: inset 12px 0px 0px $yellow;
+      border-top-right-radius: 20px;
+      border-bottom-right-radius: 20px;
+      padding: 0.5rem 1rem 0.5rem 2rem;
+
+      &.orange {
+        color: $orange;
+        box-shadow: inset 12px 0px 0px $orange;
+      }
+
+      &.blue {
+        box-shadow: inset 12px 0px 0px $blue;
+      }
+
+      &.green {
+        color: $green;
+        box-shadow: inset 12px 0px 0px $green;
+      }
     }
   }
 }
